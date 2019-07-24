d3dd9d2bf8754272e102a36d5afea6292e6d0b93
Will Rivitz <wirivitz@microsoft.com>
2019-07-24 10:47:49 -0700

    Updated attribute types and BlockParser logic

--- react-app/src/components/BlockParser/BlockParser.tsx
+++ react-app/src/components/BlockParser/BlockParser.tsx
@@ -14,51 +14,51 @@ export class BlockParser<P> extends React.Component<IBlockParserProps<P>,IBlockP
 
     private static hackyIntToCharMapping: string[] = ["a", "b", "c", "d", "e", "f", "g", "h"] // shouldn't be more than 8 args
 
-    validValueTypesToString(valueTypes: IValueType[]) {
-        var valueTypeString = "";
-        var i = 0;
-        while (i < valueTypes.length - 1) {
-            valueTypeString.concat(valueTypes[i++].toString() + " or ");
+    //deprecated in current form
+    /*
+    validValueTypesToString(valueType: IValueType) : string {
+        if (valueType === IValueType.Any) {
+            return valueType;
         }
-        valueTypeString.concat(valueTypes[i].toString());
-        return valueTypeString;
-    }
-    
-    validValueTypesToStringInStringOption(valueTypes: IValueType[]) {
-        return this.validValueTypesToString(valueTypes);
+        else {
+            return "type " + valueType;
+        }
     }
 
     validValueTypesToStringOutOfStringOption(attributes: IAttribute<P>[]) {
         var valueTypeString = ", where ";
         var i = 0;
         while (i < attributes.length - 2) {
-            valueTypeString.concat(BlockParser.hackyIntToCharMapping[i] + " is of type "
-                + this.validValueTypesToString(attributes[i].valueTypes) + ", ");
+            valueTypeString.concat(BlockParser.hackyIntToCharMapping[i] + " is of"
+                + this.validValueTypesToString(attributes[i].valueType) + ", ");
             i++;
         }
         while (i < attributes.length - 1) {
             valueTypeString.concat(BlockParser.hackyIntToCharMapping[i] + " is of type "
-                + this.validValueTypesToString(attributes[i].valueTypes) + " and ");
+                + this.validValueTypesToString(attributes[i].valueType) + " and ");
             i++;
         }
         valueTypeString.concat(BlockParser.hackyIntToCharMapping[i] + " is of type "
-            + this.validValueTypesToString(attributes[i].valueTypes));
+            + this.validValueTypesToString(attributes[i].valueType));
         return valueTypeString;
     }
+    */
 
     parsePlaceholderText(placeholderText: string, attributes: IAttribute<P>[], isInStringParse: boolean) {
         if (isInStringParse) {
             for (var i = 0; i < attributes.length; i++) {
-                placeholderText = placeholderText.replace(/{*}/, this.validValueTypesToStringInStringOption(attributes[i].valueTypes)
+                placeholderText = placeholderText.replace(/{*}/, attributes[i].valueType
                     + BlockParser.hackyIntToCharMapping[i]);
             }
         }
+        /*
         else {
             for (var i = 0; i < attributes.length; i++) {
                 placeholderText = placeholderText.replace(/{*}/, BlockParser.hackyIntToCharMapping[i]);
             }
             placeholderText.concat(this.validValueTypesToStringOutOfStringOption(attributes));
         }
+        */
         return placeholderText;
     }
 
